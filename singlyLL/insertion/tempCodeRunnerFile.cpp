d6047c6b47183f57cd41d4f2520138d51a1f981e
Mahadev1729 <devcampus17@gmail.com>
2025-01-12 12:33:05 +0530

    all setup

---
+++ singlyLL/insertion/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+Node* inserationatKth(Node* head,int val,int k){
+//     int n=count(head);
+//     Node* temp=head;
+//     if(head==NULL){
+//         if(k==1) return new Node(ele);
+//     }
+//     if(k==1){
+//      Node* temp=new Node(ele,head);
+//     }
+//     cont
+// }
