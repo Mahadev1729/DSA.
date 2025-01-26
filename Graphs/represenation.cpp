c8f2fad42d2350ef6beb8870b3b3d2b822725f35
Mahadev1729 <devcampus17@gmail.com>
2025-01-27 00:44:30 +0530

    presentaation of graph

--- Graphs/represenation.cpp
+++ Graphs/represenation.cpp
@@ -1,3 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int main(){
+    int n,m;
+    cin>>n>>m;
+    // graph array
+    int adj[n+1][n+1]={0};
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
