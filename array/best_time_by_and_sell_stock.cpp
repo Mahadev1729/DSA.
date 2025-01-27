79ea9bbbd65cdd329951f083240aa9411bb59cf5
Mahadev1729 <devcampus17@gmail.com>
2025-01-28 00:06:54 +0530

    leetcode problem 121

--- array/best_time_by_and_sell_stock.cpp
+++ array/best_time_by_and_sell_stock.cpp
@@ -2,3 +2,19 @@
 #include<vector>
 using namespace std;
 
+int buy_and_sell(vector<int>arr){
+    int n=arr.size();
+    int maxPro=0;
+    int minPrice=0;
+    for(int i=0;i<n;i++){
+        minPrice=min(minPrice,arr[i]);
+        maxPro=max(maxPro,arr[i]-minPrice);
+    }
+    return maxPro;
+}
+int main(){
+    vector<int>nums={7,1,5,3,6,4};
+    int res=buy_and_sell(nums);
+    cout<<res;
+    return 0;
+}
