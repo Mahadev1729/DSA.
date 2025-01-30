4db03cf828aaa83e77e20aea873bf4d90ea96c49
Mahadev1729 <devcampus17@gmail.com>
2025-01-30 12:38:55 +0530

    some modification

--- recursion/reversearray.cpp
+++ recursion/reversearray.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-void reverse(vector<int>arr,int l,int r){
-    if(l>=r) return;
-    swap(arr[l],arr[r]);
-    reverse(arr,l+1,r-1);
+void reverseArray(int arr[], int start, int end) {
+   
+    if (start >= end) 
+        return;
+    swap(arr[start], arr[end]);
+    reverseArray(arr, start + 1, end - 1);
 }
-int main(){
-    vector<int>nums={2,3,1,4,5};
-    reverse(nums,0,4);
-    for(auto it:nums){
-        cout<<it<<" ";
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, 0, size - 1);
+      
+      for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
     }
     return 0;
-
 }
