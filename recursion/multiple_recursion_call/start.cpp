7048a6796b90b5a144eef45b8387b3f8a5a676f8
Mahadev1729 <devcampus17@gmail.com>
2025-01-30 13:36:34 +0530

    multiple recursion call

--- recursion/multiple_recursion_call/start.cpp
+++ recursion/multiple_recursion_call/start.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int fib(int n){
     if(n<=1) return n;
-    return fib(n-1)+fib(n-2);
+    int last=fib(n-1);
+    int  slast=fib(n-2);
+    return last+slast;
+    // return fib(n-1)+fib(n-2);
 }
 int main(){
     int res=fib(5);
