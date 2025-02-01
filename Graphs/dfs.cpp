1caec21e0e18329e73d8d596cb8820ef2916da7b
Mahadev1729 <devcampus17@gmail.com>
2025-02-01 15:24:05 +0530

    code

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -1,4 +1,14 @@
 #include<iostream>
 #include<vector>
 #include<stack>
+// recursion
 
+void dfs(int node,vector<int.adj[],int vis[],vector<int>&ls){
+    vis[node]=1;
+    ls.push_back(node);
+    for(auto it:adj[node]){
+        if(!vis[it]){
+            dfs(it,adj,vis,ls);
+        }
+    }
+}
