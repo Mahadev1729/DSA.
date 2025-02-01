2003d234c668f320ea9d0cc97b40960dee89ea34
Mahadev1729 <devcampus17@gmail.com>
2025-02-01 15:50:59 +0530

    leetcode 547: no of provinces

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -3,7 +3,7 @@
 #include<stack>
 // recursion
 
-void dfs(int node,vector<int.adj[],int vis[],vector<int>&ls){
+void dfs(int node,vector<int>adj[],int vis[],vector<int>&ls){
     vis[node]=1;
     ls.push_back(node);
     for(auto it:adj[node]){
