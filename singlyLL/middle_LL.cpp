905b4809927d6d511763947204e0373e638514ae
Mahadev1729 <devcampus17@gmail.com>
2025-02-03 19:07:54 +0530

    code for count the length of LL

---
+++ singlyLL/middle_LL.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int>arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+void printll(Node* head){
+    Node * q=head;
+    while(q!=NULL){
+        cout<<q->data<<" ";
+        q=q->next;
+    }
+}
+
+int count(Node* head){
+    Node* temp=head;
+    int cnt=0;
+    while(temp!=NULL){
+        cnt++;
+        temp=temp->next;
+    }
+    return cnt;
+}
+int main(){
+    vector<int>arr={23,4,56,8};
+    Node* head=convertArr2LL(arr);
+    printll(head);
+    int res=count(head);
+    cout<<"\n";
+    cout<<res;
+    return 0;  
+}
