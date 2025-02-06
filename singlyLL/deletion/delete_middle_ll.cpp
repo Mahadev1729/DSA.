a155b2f2d1462b484625f8fc94da12d1dd4eae3f
Mahadev1729 <devcampus17@gmail.com>
2025-02-06 23:21:57 +0530

    delete middle of the linkedList

---
+++ singlyLL/deletion/delete_middle_ll.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int>arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+void printll(Node* head){
+    Node * q=head;
+    while(q!=NULL){
+        cout<<q->data<<" ";
+        q=q->next;
+    }
+}
+int main(){
+    vector<int>arr={23,4,56,8};
+    Node* head=convertArr2LL(arr);
+    printll(head);
+    return 0;  
+}
