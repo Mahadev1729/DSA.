4f6f06a92a203451365463e320e55ede6c74b31c
Mahadev1729 <devcampus17@gmail.com>
2025-02-07 23:12:01 +0530

    some modification

--- binarySearch/intro.cpp
+++ binarySearch/intro.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<vector>
+#include<sort>
+using namespace std;
 
-int binarySearch(vector<int>nums,int target){
+int binarySearch(vector<int>nums, int target){
+   
     int n=nums.size();
     int l=0;
     int r=n-1;
@@ -11,7 +14,18 @@ int binarySearch(vector<int>nums,int target){
             return mid;
         }
         else if(nums[mid]<target){
-            l=
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
         }
     }
+}
+
+int main(){
+    vector<int>arr={23,45,12,56,78};
+    sort(arr.begin(),arr.end());
+    int res=binarySearch(arr,56);
+    cout<<res;
+    return 0;
 }
