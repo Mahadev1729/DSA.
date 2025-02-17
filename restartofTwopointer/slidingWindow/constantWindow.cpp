0f6eeb988520ef406fd669b65cdfb32384051024
Mahadev1729 <devcampus17@gmail.com>
2025-02-17 21:59:30 +0530

    constant window

--- restartofTwopointer/slidingWindow/constantWindow.cpp
+++ restartofTwopointer/slidingWindow/constantWindow.cpp
@@ -1,5 +1,30 @@
 #include<iostream>
 #include<vector>
+#include <climits>
+
 using namespace std;
 
+int  constantWindow(vector<int>arr,int k){
+    int n=arr.size();
+    int l=0;
+    int r=k-1;
+    int sum=0;
+    int maxSum=INT_MAX;
+    for(int i=0;i<r;i++){
+        sum+=arr[i];
+    }
+    while(r<n-1){
+       sum-=arr[l];
+       l++;
+       r++;
+       sum+=arr[n];
+       maxSum=max(maxSum,sum);
+    }
+    return maxSum;
+}
 
+int main(){
+    vector<int>nums={-1,2,3,4,5,-1};
+    cout<<constantWindow(nums,4);
+    return 0;
+}
