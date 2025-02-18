4b9926f9f68e36b9e773f934a76774144c2aecff
Mahadev1729 <devcampus17@gmail.com>
2025-02-18 22:31:57 +0530

    code for shrink and expend

--- restartofTwopointer/longestSubarray.cpp
+++ restartofTwopointer/longestSubarray.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
 
 
+int longestSubarray(vector<int>arr,int k){
+    int n=arr.size();
+    int r=0;
+    int l=0;
+    int sum=0;
+    int maxLen=0;
+    while (r<n)
+    {
+        sum+=arr[r];
+        while(sum>k){
+            sum-=arr[r];
+            l+=1;
+        }
+        if(sum<=k){
+            maxLen=max(maxLen,r-l+1);
+        }
+        r+=1;
+    }
+    return maxLen;
+}
 
+int main(){
+    vector<int>nums={2,5,1,7,10};
+    cout<<longestSubarray(nums,14);
+    return 0;
+}
