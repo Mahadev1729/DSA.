86096a8c513372e6c3cfde681674025cad7c7fe9
Mahadev1729 <devcampus17@gmail.com>
2025-02-23 13:36:40 +0530

    hashMap concept

---
+++ Hashing/Maps/basicMap.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void hashMap(vector<int>arr){
+    map<int,int>mp;
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        mp[arr[i]]++;
+    }
+    for(auto it:mp){
+        cout<<it.first<<":"<<it.second<<endl;
+    }
+}
+
+int main(){
+    vector<int>nums={1,2,3,1,12};
+    hashMap(nums);
+    return 0;
+}
