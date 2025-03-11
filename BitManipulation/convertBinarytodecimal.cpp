e770908ec86de1439624446407bba31b98b43e22
Mahadev1729 <devcampus17@gmail.com>
2025-03-11 19:03:59 +0530

    convert binary to decimal

---
+++ BitManipulation/convertBinarytodecimal.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int convertDecimaltoBinary(string bS){
+    reverse(bS.begin(),bS.end());
+    int n=bS.length();
+    int dS=0;
+    int i;
+    for(i=0;i<n;i++){
+       dS+=bS[i]*pow(2,i);
+    }
+    return dS;
+}
+
+int main()
+{
+    string inputString;
+    cout<<"Enter the input string:";
+    cin>>inputString;
+    int ans=convertDecimaltoBinary(inputString);
+    cout<<ans;
+    return 0;
+}
