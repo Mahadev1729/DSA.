f02195f10a70a38d17e9435194b22c62192c314c
Mahadev1729 <devcampus17@gmail.com>
2025-03-11 23:35:09 +0530

    gfg question

--- BitManipulation/ithbitissetORnot.cpp
+++ BitManipulation/ithbitissetORnot.cpp
@@ -3,3 +3,13 @@
 using namespace std;
 
 
+class Solution {
+  public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k) {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+         if((n &(1<<k))!=0) return true;
+         else return false;
+    }
+};
