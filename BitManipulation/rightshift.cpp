6f3a363a19457f2d55a088db009297925555c0f0
Mahadev1729 <devcampus17@gmail.com>
2025-03-11 19:28:22 +0530

    right shift

--- BitManipulation/rightshift.cpp
+++ BitManipulation/rightshift.cpp
@@ -2,4 +2,27 @@
 
 using namespace std;
 
-int convert
+int binaryToDecimal(const string& binary) {
+    int decimal = 0;
+    int length = binary.length();
+    
+    for (int i = 0; i < length; i++) {
+        if (binary[length - 1 - i] == '1') {
+            decimal += pow(2, i);
+        }
+    }
+    return decimal;
+}
+
+string convert2Binary(int n){
+    string res="";
+    while(n!=1){
+      if(n%2==1) res+='1';
+      else res+='0';
+      n=n/2;  
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
+
+int rightShift( )
