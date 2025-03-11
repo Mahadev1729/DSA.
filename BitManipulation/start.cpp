fcf89acf1897a6b2980934877b3d571e14ef248e
Mahadev1729 <devcampus17@gmail.com>
2025-03-11 15:41:56 +0530

    convert decimal to binary

--- BitManipulation/start.cpp
+++ BitManipulation/start.cpp
@@ -2,4 +2,20 @@
 
 using namespace std;
 
+// creating function
+string convert2Binary(int n){
+    string res="";
+    while(n!=1){
+      if(n%2==1) res+='1';
+      else res+='0';
+      n=n/2;  
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
 
+int main(){
+    string sol=convert2Binary(2);
+    cout<<sol;
+    return 0;
+}
