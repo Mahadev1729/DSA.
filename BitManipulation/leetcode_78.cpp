b59bed83e187e748f5bc5f0499638988e6cd49d5
Mahadev1729 <devcampus17@gmail.com>
2025-03-12 22:12:25 +0530

    power set

---
+++ BitManipulation/leetcode_78.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// number of powerset=2**n 
+// using bit mainpulation
+
+
+vector<int> powerset(vector<int>arr){
+    int n=arr.size();
+    subset=1<<n;
+    vector<int>ans;
+    for(int num=0;nums<subset-1;num++){
+        vector<int>list={};
+        for(int i=0;i<n;i++){
+            if((num&(1<<i))){
+                list.push_back(arr[i]);
+            }
+        }
+        ans.push_back(list);
+    }
+    return ans;
+    
+}
