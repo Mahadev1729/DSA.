0300581b0361a77152b5c302c28f1fb844627b0d
Mahadev1729 <devcampus17@gmail.com>
2025-03-15 19:31:29 +0530

    lowerbound code

--- binarySearch/loweerBound.cpp
+++ binarySearch/loweerBound.cpp
@@ -1,9 +1,17 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// smallest index such that arr[mid]>=x
-
-int main(){
+int main() {
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int x = 12;
     
+    // Using lower_bound to find the smallest index where arr[mid] >= x
+    auto it = lower_bound(arr.begin(), arr.end(), x);
+    
+    if (it != arr.end()) 
+        cout << "Lower bound: " << *it << endl;
+    else 
+        cout << "No lower bound found" << endl;
+
+    return 0;
 }
