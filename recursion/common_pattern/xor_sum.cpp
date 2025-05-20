c96b87ef45c9d3ce9f73ec2a84c710914423f015
Mahadev1729 <devcampus17@gmail.com>
2025-05-20 12:07:27 +0530

    xor sum subset

---
+++ recursion/common_pattern/xor_sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public:
+        int subsetXORSum(vector<int>& nums) {
+            int totalSum=0;
+            subsetXorSum(0,0,nums,totalSum);
+            return totalSum;
+        }
+    
+    void subsetXorSum(int index, int currentXor, vector<int>& nums, int& totalSum) {
+        int n = nums.size();
+        if (index == n) {
+            totalSum += currentXor;
+            return;
+        }
+        
+        
+        subsetXorSum(index + 1, currentXor ^ nums[index], nums, totalSum);
+        
+        
+        subsetXorSum(index + 1, currentXor, nums, totalSum);
+    }
+    };
