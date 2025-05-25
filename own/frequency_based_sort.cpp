342fc20b9be01c5ce6e2a55d5ad4da894f8a611b
Mahadev1729 <devcampus17@gmail.com>
2025-05-25 09:29:59 +0530

    frequency based sorting

---
+++ own/frequency_based_sort.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int a, int b, unordered_map<int, int>& freq) {
+    if (freq[a] == freq[b])
+        return a < b; // Tie-breaker: by value
+    return freq[a] < freq[b]; // Lower frequency first
+}
+
+int main() {
+    vector<int> arr = {4, 5, 6, 5, 4, 3};
+    unordered_map<int, int> freq;
+
+    for (int num : arr)
+        freq[num]++;
+
+    sort(arr.begin(), arr.end(), [&](int a, int b) {
+        return compare(a, b, freq);
+    });
+
+    for (int num : arr)
+        cout << num << " ";
+
+        return 0;
+}
