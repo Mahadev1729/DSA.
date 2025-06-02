017abf051cd1095f2ae3547bc1550921fed48928
Mahadev1729 <devcampus17@gmail.com>
2025-06-02 16:05:32 +0530

    stack implmenation using linkedlist

---
+++ stacks_dsa/with_ll.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class Stack {
+    Node* top;
+
+public:
+    Stack() { top = nullptr; }
+
+    void push(int x) {
+        Node* temp = new Node();
+        temp->data = x;
+        temp->next = top;
+        top = temp;
+    }
+
+    void pop() {
+        if (top == nullptr) {
+            cout << "Stack is empty\n";
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    void display() {
+        Node* temp = top;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << "\n";
+    }
+};
+
+int main() {
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.display(); 
+    s.pop();
+    s.display();  
+    return 0;
+}
