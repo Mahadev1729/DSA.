1f2eb8bcf59b5834b2f0956ab82d8eccf4b7e727
Mahadev1729 <devcampus17@gmail.com>
2025-07-25 19:11:07 +0530

    Add initial implementation of try.cpp with sample 2D vector and output

---
+++ 2DMatrix/try.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Sample 2D vector
+    vector<vector<int>> grid = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    for(auto it:grid[1]){
+        cout<<it;
+    }
+
+    return 0;
+}
