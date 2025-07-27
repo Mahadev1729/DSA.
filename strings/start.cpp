2ca18bced216e7281bacf02507e11b97f6754e63
Mahadev1729 <devcampus17@gmail.com>
2025-07-27 16:46:58 +0530

    Add initial implementation of start.cpp for character array input and output

---
+++ strings/start.cpp
@@ -0,0 +1,15 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    // char str[]={'a','b','c','\0'};
+    // int arr[]={1,2,3};
+    // cout<<arr<<endl;
+    // cout<<str;
+    char str[100];
+    cout<<"Enter character array:";
+    cin.getline(str,100);
+    cout<<str<<endl;
+    return 0;
+}
