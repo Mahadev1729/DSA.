3a0757154d2b8f6d04511dc5f56a120b3e4f643a
Mahadev1729 <devcampus17@gmail.com>
2025-07-27 09:30:23 +0530

    Add initial implementation of Container With Most Water and string permutation algorithms

---
+++ leetCode150/Container _With_Most_Water.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0;
+        int r=height.size()-1;
+        int maxArea=INT_MIN;
+        while(l<r){
+            int area=min(height[l],height[r])*(r-l);
+
+            maxArea=max(area,maxArea);
+            if(height[l]<height[r])
+              l++;
+            else
+              r--;
+        }
+        return maxArea;
+    }
+};
---
+++ leetCode150/string_permuation.cpp
@@ -0,0 +1,20 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+void string_permute(string s,int index){
+    if(index==s.size()){
+        cout<<s<<endl;
+        return;
+    }
+    for(int i=index;i<s.size();i++){
+        swap(s[index],s[i]);
+        string_permute(s,index+1);
+        swap(s[index],s[i]);
+    }
+}
+
+int main(){
+    string s="abcd";
+    string_permute(s,0);
+    return 0;
+}
