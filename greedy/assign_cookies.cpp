c3a486ba3af9db468fae06300e1badbb2f9dbd1e
Mahadev1729 <devcampus17@gmail.com>
2025-07-30 00:25:44 +0530

    Add initial implementations of findContentChildren and matchPlayersAndTrainers algorithms

---
+++ greedy/assign_cookies.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int l=0;
+        int r=0;
+        int n=g.size();
+        int m=s.size();
+        while(l<m && r<n){
+            if(g[r]<=s[l]){
+                r++;
+            }
+            l++;
+        }
+        return r;
+
+    }
+};
---
+++ greedy/maximu_player_with_trainer.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        int l=0,r=0;
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int n=players.size();
+        int m=trainers.size();
+        int count=0;
+        while(l<m && r<n){
+            if(players[r]<=trainers[l]){
+                r++;
+            }
+            l++;
+        }
+        return r;
+    }
+};
