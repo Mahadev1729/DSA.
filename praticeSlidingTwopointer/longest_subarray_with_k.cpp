9fc4cf25e6599bd6ab38f3a78ac90172ba71e4ba
Mahadev1729 <devcampus17@gmail.com>
2025-08-02 22:49:07 +0530

    constant window size and varible size k

---
+++ praticeSlidingTwopointer/longest_subarray_with_k.cpp
@@ -0,0 +1,36 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+// input array=[2,5,1,7,10]
+// sum=14
+
+int max_subarrayKsum(vector<int>&nums,int kSum){
+    int n=nums.size();
+    int sum=0;
+    int r=0;
+    int l=0;
+    int maxLen=INT_MIN;
+    while(r<n){
+        sum+=nums[r];
+        while(sum>kSum){
+            sum-=nums[l];
+            l++;
+        }
+
+        if(sum<=kSum){
+            maxLen=max(maxLen,r-l+1);
+        }
+        r++;
+
+    }
+    return maxLen;
+}
+
+int main(){
+    vector<int>arr={-1,3,4,5,6,7};
+    int res=max_subarrayKsum(arr,14);
+    cout<<res;
+    return 0;
+
+}
--- praticeSlidingTwopointer/start.cpp
+++ praticeSlidingTwopointer/start.cpp
@@ -1,1 +1,28 @@
-﻿
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+
+int  constantWindow(vector<int>arr,int k){
+    int n=arr.size();
+    int l=0;
+    int r=k-1;
+    int sum=0;
+    int maxSum=INT_MIN;
+    for(int i=0;i<=r;i++){
+        sum+=arr[i];
+    }
+    while(r<n-1){
+       sum-=arr[l];
+       l++;
+       r++;
+       sum+=arr[r];
+       maxSum=max(maxSum,sum);
+    }
+    return maxSum;
+}
+
+int main(){
+    vector<int>nums={-1,2,3,4,5,-1};
+    cout<<constantWindow(nums,4);
+    return 0;
+}
