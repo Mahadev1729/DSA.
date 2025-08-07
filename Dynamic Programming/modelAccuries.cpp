17a6007c5bcae775786180e48adb9bcaf5cbf4a2
Mahadev1729 <devcampus17@gmail.com>
2025-08-08 00:44:32 +0530

    dp problem

---
+++ Dynamic Programming/modelAccuries.cpp
@@ -0,0 +1,31 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+void countSubSets(int indx,vector<int>&valid,int minModels,int count,int& total){
+    if(indx==valid.size()){
+        if(count>=minModels)
+            total++;            
+        return;
+    }
+    countSubSets(indx+1,valid,minModels,count+1,total);
+    countSubSets(indx+1,valid,minModels,count,total);
+}    
+
+int main(){
+    vector<int>modelAccuries={12,4,6,13,5,10};
+    int minModels=3;
+    int minAccuracy=4;
+    int maxAccuracy=10;
+    vector<int>valid;
+    for(auto acc:modelAccuries){
+        if(acc>=minAccuracy && acc<=maxAccuracy){
+            valid.push_back(acc);
+        }
+    }
+    int total=0;
+    countSubSets(0,valid,minModels,0,total);
+    cout<<total;
+    return 0;
+}
+
+
