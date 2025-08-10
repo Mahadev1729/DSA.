1bfc1a6864dc3edfb7d5cec2b5c92743b5a9a0f6
Mahadev1729 <devcampus17@gmail.com>
2025-08-10 23:11:24 +0530

    graph

---
+++ revisitGraph/BFS.cpp
@@ -0,0 +1,5 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+  
+
---
+++ revisitGraph/storeInList.cpp
@@ -0,0 +1,16 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+// store using list
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int>adj[n+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        adj[u].push_back(v);
+        // adj[v].push_back(u);// directed graph
+
+    }
+    return 0;
+}
