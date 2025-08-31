4673295530485cddae478629af4d890494a49125
Mahadev1729 <devcampus17@gmail.com>
2025-08-31 19:34:14 +0530

    Implement insertion sort algorithm and add related executable files

--- sorting/insertionsort.cpp
+++ sorting/insertionsort.cpp
@@ -1,1 +1,28 @@
-﻿
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+void printArray(vector<int>&arr){
+    for(auto it:arr){
+        cout<<it<<" ";
+    }
+}
+
+void insertionSort(vector<int>&nums,int n){
+    
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && nums[j-1]>nums[j]){
+            swap(nums[j-1],nums[j]);
+            j--;
+        }
+    }
+
+    printArray(nums);
+}
+
+int main(){
+vector<int>a={14,9,15,12,6,8,13};
+insertionSort(a,a.size());
+return 0;
+}
