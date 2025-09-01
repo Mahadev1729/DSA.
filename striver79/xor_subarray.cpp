dedb70b3ed7311332d4ae3324aa15cbf1e8541ad
Mahadev1729 <devcampus17@gmail.com>
2025-09-02 00:29:01 +0530

    Update launch configuration and add XOR subarray implementation with executables

--- striver79/xor_subarray.cpp
+++ striver79/xor_subarray.cpp
@@ -3,9 +3,22 @@
 using namespace std;
 
 int count_subarray_xor(vector<int>&nums,int k){
-   
+   int xr=0;
+   map<int,int>mpp;
+   mpp[xr]++;
+   int cnt=0;
+   for(int i=0;i<nums.size();i++){
+    xr=xr^nums[i];
+    int x=xr^k;
+    cnt+=mpp[x];
+    mpp[xr]++;
+   }
+   return cnt;
 }
 
 int main(){
-
+    vector<int>arr={4,2,2,6,4};
+    int res=count_subarray_xor(arr,6);
+    cout<<res;
+    return 0;
 }
