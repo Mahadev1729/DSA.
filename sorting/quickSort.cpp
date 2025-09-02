961a8de8e546ba061bcf41e3de6f682a33a1a31c
Mahadev1729 <devcampus17@gmail.com>
2025-09-03 00:07:00 +0530

    Add QuickSort implementation and executable

---
+++ sorting/quickSort.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (i <= high && arr[i] <= pivot) i++;
+        while (j >= low && arr[j] > pivot) j--;
+
+        if (i < j) swap(arr[i], arr[j]);
+    }
+
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+int main() {
+    vector<int> nums = {2, 1, 3, 4, 5, 7, 9, 1, 3};
+    qs(nums, 0, nums.size() - 1);
+
+    for (auto it : nums) {
+        cout << it << " ";
+    }
+    return 0;
+}
