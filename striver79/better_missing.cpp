ed794f80b699909c8b9d866a7b3121dacacbb091
Mahadev1729 <devcampus17@gmail.com>
2025-09-04 00:57:19 +0530

    Add implementations for missing number and repeating number algorithms; include executables

---
+++ striver79/better_missing.cpp
@@ -0,0 +1,32 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+
+vector<int> missing_rep(vector<int>&arr){
+    int n=arr.size();
+    vector<int>hash(n+1,0);
+    for(int i=0;i<n;i++){
+        hash[arr[i]]++;
+    }
+    int rep=-1,miss=-1;
+    for(int i=1;i<=n;i++){
+        if(hash[i]==2) rep=i;
+        else if(hash[i]==0) miss=i;
+
+        if(rep!=-1 && miss!=-1){
+            break;
+        }
+    }
+    return {rep,miss};
+}
+
+
+int main() {
+    vector<int> nums = {4, 3, 6, 2, 1, 1};
+    vector<int> res = missing_rep(nums);
+    for (auto it : res) {
+        cout << it << " ";
+    }
+    return 0;
+}
---
+++ striver79/missing_repeting.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> missing_repeating(vector<int> arr) {
+    int n = arr.size();
+    int repeating = -1, missing = -1;
+
+    for (int i = 1; i <= n; i++) {
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[j] == i) count++;  
+        }
+        if (count == 2) repeating = i;
+        else if (count == 0) missing = i;
+    }
+    return {repeating, missing};
+}
+
+int main() {
+    vector<int> nums = {4, 3, 6, 2, 1, 1};
+    vector<int> res = missing_repeating(nums);
+    for (auto it : res) {
+        cout << it << " ";
+    }
+    return 0;
+}
