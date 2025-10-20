70f492fde844323096b183fe873d52c04efafdee
Mahadev1729 <devcampus17@gmail.com>
2025-10-20 16:35:43 +0530

    Add implementations for longest substring without repeating characters and optimal substring with repeating characters; include executables

---
+++ revisitTree/rep.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+class BinaryTree{
+    int data;
+    BinaryTree* left;
+    BinaryTree* right;
+    BinaryTree(int val){
+        data=val;
+        left=right=nullptr;
+    } 
+};
+void display(BinaryTree* bt){
+    if(bt==NULL) return NULL;
+    cout<<b->data<<endl;
+    display(bt->left);
+    display(bt->right)
+}
+
+
+int main(){
+    BinaryTree root=new BinaryTree(1);
+    root->left=new BinaryTree(2);
+    root->right=new BinaryTree(3);
+    root->left->left=new BinaryTree(4);
+    root->left->right=new BinaryTree(5);
+    display(root);
+    return 0;
+}
---
+++ strings/optimal_repeatstring.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int optimal_wth_repeatstring(string s){
+  vector<int>hash(256,-1);
+  int n=s.size();
+  int l=0;
+  int r=0;
+  int maxLen=0;
+  while(r<n){
+    if(hash[s[r]]!=-1){
+        if(hash[s[r]]>=l){
+            l=hash[s[r]]+1;
+        }
+    }
+    maxLen=max(maxLen,r-l+1);
+    hash[s[r]]=r;
+    r++;
+  }
+  return maxLen;
+}
+
+int main(){
+ string s="cadbzabcd";
+ int res=optimal_wth_repeatstring(s);
+ cout<<res;
+ return 0;
+}
