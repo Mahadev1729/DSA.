445ac64bcf1b9c86094e3792bc0c41e633ded742
Mahadev1729 <devcampus17@gmail.com>
2025-10-20 19:12:32 +0530

    Add breadth-first search implementation for binary tree; include executable

---
+++ revisitTree/bfstree.cpp
@@ -0,0 +1,69 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+class TreeNode{
+    public:
+    int data;
+    TreeNode* right;
+    TreeNode* left;
+    TreeNode(int value){
+        data=value;
+        right=left=nullptr;
+        
+    }
+};
+int eleSum(vector<int>&arr){
+    int sum=0;
+    for(auto it:arr){
+        sum+=it;
+    } 
+    return sum;
+}
+vector<int> levelorder(TreeNode* root){
+    vector<int> ans;
+    if(root==NULL) return ans;
+    queue<TreeNode*>q;
+    q.push(root);
+    while(!q.empty()){
+        int size=q.size();
+        vector<int>level;
+        for(int i=0;i<size;i++){
+            TreeNode *node=q.front();
+            q.pop();
+            if(node->left!=NULL)q.push(node->left);
+            if(node->right!=NULL)q.push(node->right);
+            level.push_back(node->data);
+        }
+        ans.push_back(eleSum(level));
+    }
+    return ans;
+}
+
+
+int main(){
+    TreeNode* root=new TreeNode(10);
+    // 1st level
+    root->left=new TreeNode(12);
+    root->right=new TreeNode(13);
+
+    // 2nd level
+
+    root->left->left=new TreeNode(14);
+    root->left->right=new TreeNode(15);
+    
+    // vector<vector<int>>vec=levelorder(root);
+    // for (const auto& row : vec) {         
+    //     for (const auto& val : row) {      
+    //         cout << val << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    vector<int>ans=levelorder(root);
+    for(auto it:ans){
+        cout<<it<<endl;
+    }
+    return 0;
+
+}
