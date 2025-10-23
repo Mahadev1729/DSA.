776389bcbbb62d2d91362b20ecfae2bd6597abd1
Mahadev1729 <devcampus17@gmail.com>
2025-10-24 00:04:45 +0530

    Add implementations for various string algorithms and their executables

---
+++ strings/validAnograms.cpp
@@ -0,0 +1,18 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+bool validAnograms(string s,string t){
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+
+    return (s==t);
+}
+
+int main(){
+    string s="naman";
+    string t="amnan";
+    bool flag=validAnograms(s,t);
+    cout<<flag;
+    return 0;
+}
---
+++ strings/valid_anagram_optimal.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+
+bool solve(string s,string t){
+    unordered_map<char,int>mp;
+    for(auto it:s){
+        mp[it]++;
+    }
+    for(auto it:t){
+        mp[it]--;
+    }
+    for(auto it:mp){
+        if(it.second==0){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    string s="eat";
+    string t="te";
+    bool res=solve(s,t);
+    cout<<res;
+    return 0;
+}
