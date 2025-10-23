8e57520b355f59b90490e8f93f32248fb562d2a8
Mahadev1729 <devcampus17@gmail.com>
2025-10-24 00:46:41 +0530

    Implement selection sort algorithm and add executable; remove unused test files

--- revisitSorting/selectionsort.cpp
+++ revisitSorting/selectionsort.cpp
@@ -1,1 +1,28 @@
-﻿
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+void selectionsort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i <= n - 2; i++) {
+        int mini = i;
+        for (int j = i + 1; j < n; j++) { 
+            if (arr[j] < arr[mini]) {
+                mini = j;
+            }
+        }
+        swap(arr[mini], arr[i]); 
+    }
+}
+
+void printArr(vector<int>& resultArr) {
+    for (auto it : resultArr)
+        cout << it << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> nums = {13, 46, 24, 52, 20, 9};
+    selectionsort(nums);
+    printArr(nums);
+    return 0;
+}
