ab623b05cf0dc1ef0bcabca65edeb4ebc2bd45a1
Mahadev1729 <devcampus17@gmail.com>
2025-10-24 19:03:16 +0530

    Add bubble sort implementation and executable

---
+++ revisitSorting/bubbleSort.cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+void bubbleSort(vector<int>&arr){
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[j]>arr[j+1]){
+            swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(auto it:arr){
+        cout<<it<<" ";
+    }
+}
+
+int main(){
+    vector<int>arr={23,45,12,34};
+    bubbleSort(arr);
+    return 0;
+}
