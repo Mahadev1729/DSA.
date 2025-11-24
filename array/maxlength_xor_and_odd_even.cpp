0493eb6d7ea5280dc77fcf177738599c297e8efd
Mahadev1729 <devcampus17@gmail.com>
2025-11-24 12:13:54 +0530

    Update launch configuration for greedy algorithms; add implementations for lemonade change and maximum length of subarrays with odd/even counts

---
+++ array/maxlength_xor_and_odd_even.cpp
@@ -0,0 +1,46 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+bool has_Odd_Even(vector<int>&arr){
+    int evenCount=0;
+    int oddCount=0;
+    for(int ele:arr){
+        if(ele%2!=0){
+            evenCount++;
+        }else{
+            oddCount++;
+        }
+    }
+    return (evenCount==oddCount);
+}
+
+bool is_Xor_Zero(vector<int>&arr){
+    int xor_ele=0;
+    for(auto it:arr){
+        xor_ele=xor_ele^it;
+    }
+    return (xor_ele==0);
+}
+
+int generate_allsubarr(vector<int>&arr){
+    int n=arr.size();
+    int maxLen=INT_MIN;
+    for(int i=0;i<n;i++){
+        vector<int>subArr;
+        for(int j=i;j<n;j++){
+           subArr.push_back(arr[j]);
+           if(has_Odd_Even(subArr) && is_Xor_Zero(subArr)){
+            maxLen=max(maxLen,(int)subArr.size());
+           }
+        }
+    }
+    return maxLen;
+}
+
+int main(){
+   vector<int>nums={3,2,8,5,4,14,9,15};
+   int res=generate_allsubarr(nums);
+   cout<<res;
+   return 0;
+}
---
+++ greedy/lamnodeChange.cpp
@@ -0,0 +1,38 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+
+        for (int b : bills) {
+            if (b == 5) {
+                five++;
+            } 
+            else if (b == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } 
+            else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } 
+                else if (five >= 3) {
+                    five -= 3;
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+int main(){
+    vector<int>arr={5,5,5,10,20};
+    bool res=lemonadeChange(arr);
+    cout<<res;
+    return 0;
+}    
+
