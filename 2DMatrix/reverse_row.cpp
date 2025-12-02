f54b0197491dfe0a37ed1cc2701c9746c1221839
Mahadev1729 <devcampus17@gmail.com>
2025-12-02 15:41:48 +0530

    Update launch configuration and add implementations for image flipping and KMP string search algorithms

---
+++ 2DMatrix/reverse_row.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> flipAndInvertImage(vector<vector<int>>& images) {
+        for(auto &row:images){
+            reverse(row.begin(),row.end());
+        }
+        return images;
+    } 
+    
+    
+int main(){
+  vector<vector<int>>arr={
+    {1,2,3,4},
+    {5,6,7,8},
+    {10,11,12,13},
+    {14,15,16,17}
+    };
+
+     vector<vector<int>>res=flipAndInvertImage(arr);
+     for (const auto &row : res) {        
+        for (int val : row) {            
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}    
---
+++ strings/KMP.cpp
@@ -0,0 +1,52 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> buildLPS(const string &pattern) {
+    int n = pattern.size();
+    vector<int> lps(n, 0);
+    int len = 0, i = 1;
+
+    while (i < n) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+
+void KMPSearch(const string &text, const string &pattern) {
+    int n = text.size();
+    int m = pattern.size();
+    vector<int> lps = buildLPS(pattern);
+    int i = 0, j = 0;
+
+    while (i < n) {
+        if (text[i] == pattern[j]) {
+            i++;
+            j++;
+        }
+        if (j == m) {
+            cout << "Pattern found at index " << i - j << "\n";
+            j = lps[j - 1];
+        } else if (i < n && text[i] != pattern[j]) {
+            if (j != 0) j = lps[j - 1];
+            else i++;
+        }
+    }
+}
+
+int main() {
+    string text = "ababcabcabababd";
+    string pattern = "ababd";
+    KMPSearch(text, pattern);
+    return 0;
+}
