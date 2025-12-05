6f83215672451145870ae69133ae5623b57d627e
Mahadev1729 <devcampus17@gmail.com>
2025-12-05 22:06:06 +0530

    Add implementations for asteroid collision and even sum difference algorithms; include main function for testing

---
+++ array/astroid_collision.cpp
@@ -0,0 +1,31 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> asteroidCollision(vector<int>&a){
+    int n=a.size();
+    vector<int>list;
+    for(int i=0;i<n;i++){
+        if(a[i]>0) list.push_back(a[i]);
+        else{
+            while(!list.empty() && list.back()>0 && list.back()<abs(a[i])){
+                list.pop_back();
+            }
+        }
+        if(list.empty() && list.back()==abs(a[i])){
+            list.pop_back();
+        }else if(list.empty() || list.back()<0){
+            list.push_back(a[i]);
+        }
+    }
+    return list;
+}
+
+int main(){
+    vector<int>arr={4,7,1,1,2,-3,-7,17,15,-18,-19};
+    vector<int>res=asteroidCollision(arr);
+    for(int it:res){
+        cout<<it<<" ";
+    }
+    return 0;
+}
---
+++ array/even_sum_diff.cpp
@@ -0,0 +1,32 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int countPartitions(vector<int>& nums) {
+        int totalSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            totalSum += nums[i]; 
+        }
+        int leftSum = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size() - 1; i++) { 
+            leftSum += nums[i]; 
+            int rightSum = totalSum - leftSum; 
+
+            if ((leftSum % 2) == (rightSum % 2)) {
+                count++; 
+            }
+        }
+        return count; 
+    }
+
+int main(){
+    vector<int>arr={10,10,3,7,6};
+    int res=countPartitions(arr);
+    
+    cout<<res;
+    return 0;
+
+}    
