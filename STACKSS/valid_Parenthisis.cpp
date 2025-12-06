c3f836b35b10efe700e987a762484b87ce676195
Mahadev1729 <devcampus17@gmail.com>
2025-12-07 00:00:04 +0530

    Implement valid parenthesis checker; add main function for testing

--- STACKSS/valid_Parenthisis.cpp
+++ STACKSS/valid_Parenthisis.cpp
@@ -1,12 +1,36 @@
 ﻿#include<bits/stdc++.h>
-
 using namespace std;
 
-bool is_Valid_Parenthisis(string s){
-    
+bool is_Valid_Parenthisis(string s) {
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++) {
+        char ch = s[i];
+
+        if (ch == '(' || ch == '{' || ch == '[') {
+            st.push(ch);
+        } else {
+            if (!st.empty()) {
+                char top = st.top();
+
+                if ((ch == ')' && top == '(') ||
+                    (ch == '}' && top == '{') ||
+                    (ch == ']' && top == '[')) {
+
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+    return st.empty();
 }
 
-
-int main(){
-
+int main() {
+    string input = "{([])}";
+    bool res = is_Valid_Parenthisis(input);
+    cout << res;
+    return 0;
 }
