4da860638e6a3bf084718d55856cdf8a739498aa
Mahadev1729 <devcampus17@gmail.com>
2025-12-11 23:28:34 +0530

    Implement optimal solution for next greater element using stack; add string compression function with main for testing

--- STACKSS/NGE.cpp
+++ STACKSS/NGE.cpp
@@ -17,9 +17,27 @@ vector<int> next_Greater_Ele(vector<int>&arr){
    }
    return nge;
 }
+
+// optimal solution
+// use of stack
+vector<int> findNextGreterELe(vector<int>&arr){
+    vector<int>nge(arr.size());
+    stack<int>st;
+    for(int i=arr.size()-1;i>0;i--){
+        while(!st.empty() && st.top()<=arr[i]){
+            st.pop();
+        }
+        if(st.empty()) nge[i]=-1;
+        else nge[i]=st.top();
+        st.push(arr[i]);
+    }
+    return nge;
+}
+
+
 int main(){
     vector<int>arr={6,0,8,1,3};
-    vector<int>res=next_Greater_Ele(arr);
+    vector<int>res=findNextGreterELe(arr);
     for(int it:res){
         cout<<it<<" ";
     }
---
+++ strings/stringCompress.cpp
@@ -0,0 +1,36 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+vector<char> compress(vector<char>& chars) {
+        int n=chars.size();
+        int indx=0;
+        for(int i=0;i<n;i++){
+            char ch=chars[i];
+            int count=0;
+            while(i<n && chars[i]==ch){
+                count++;
+                i++;
+            }
+            if(count==1){
+                chars[indx++]=ch;
+            }else{
+                chars[indx++]=ch;
+                string str=to_string(count);
+                for(char dig:str){
+                    chars[indx++]=dig;
+                }
+            }
+            i--;
+        }
+        chars.resize(indx);
+        return chars;
+}
+
+int main(){
+    vector<char>arr={'a','a','b','b','c'};
+    vector<char> res=compress(arr);
+    for(char ch:res){
+        cout<<ch<<" ";
+    }
+    return 0;
+}
