00f4a800b8ad13ba92d2847b3275ea24f87e0081
Mahadev1729 <devcampus17@gmail.com>
2025-12-27 00:52:59 +0530

    Add implementation for string rotation check; include main function for testing

--- strings/rotation.cpp
+++ strings/rotation.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 bool solve(string s,string goal){
-    
+    int m=s.length();
+    int n=goal.length();
+    if(m!=n) return false;
+    return (s+s).find(goal)!=string::npos;
+}
+int main(){
+    string s="abcde";
+    string g="cdeab";
+    cout<<solve(s,g);
+    return 0;
 }
