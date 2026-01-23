14e842383dc1869b978e55de487c745c07a50fd4
Mahadev1729 <devcampus17@gmail.com>
2026-01-24 02:20:11 +0530

    Add new pattern solving functions and update main execution flow

--- PatternsUsingLoop/start.cpp
+++ PatternsUsingLoop/start.cpp
@@ -62,7 +62,79 @@ void solve3(int n){
     }
 }
 
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+void solve4(int n){
+   for(int i=0;i<n;i++){
+    for(int j=1;j<n-i+1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+   }
+}
+
+//       *
+//     * * *
+//   * * * * *   
+
+void solve5(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+             cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+
+}
+
+void solve6(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+             cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+// *
+// **
+// ***
+// ****
+// ***
+// **
+// *
+
+void solve7(int n){
+    for(int i=0;i<2*n-1;i++){
+        int stars=i;
+        if(i>n) stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
 int main(){
-    solve2(5 );
+    // solve5(5);
+    // solve6(5);
+    solve7(5);
     return 0;
 }
