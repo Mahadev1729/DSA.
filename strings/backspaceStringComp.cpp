f4e473b48f497cff07cae490159ff1e4dc74333f
Mahadev1729 <devcampus17@gmail.com>
2026-01-23 23:15:10 +0530

    Add initial implementation of backspace string comparison function

---
+++ strings/backspaceStringComp.cpp
@@ -0,0 +1,15 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+bool solve(string s,string t){
+    string s1;
+    string s2;
+    for(int i=0;i<s.length();i++){
+        if(s[i])
+    }
+}
+
+int main(){
+    
+}
