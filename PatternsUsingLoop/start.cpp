bacbe5fd362a605d09ab33583adde5a83e160238
Mahadev1729 <devcampus17@gmail.com>
2026-01-23 23:20:47 +0530

    Add initial implementation of a pattern printing program with executable

---
+++ PatternsUsingLoop/start.cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+
+
+using namespace std;
+
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+void solve(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    solve();
+    return 0;
+}
