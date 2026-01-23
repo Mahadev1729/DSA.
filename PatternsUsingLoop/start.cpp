60efaf83d3e112ec8d17d098099aacaca1a1b026
Mahadev1729 <devcampus17@gmail.com>
2026-01-24 00:00:50 +0530

    Add pattern printing functions and update main execution flow

--- PatternsUsingLoop/start.cpp
+++ PatternsUsingLoop/start.cpp
@@ -17,7 +17,52 @@ void solve(){
     }
 }
 
+
+// *
+// * *
+// * * * *
+// * * * * *
+
+void solve1(){
+      for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+      }
+}
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4 
+// 1 2 3 4 5
+
+void solve2(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+// 1 
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+void solve3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
-    solve();
+    solve2(5 );
     return 0;
 }
