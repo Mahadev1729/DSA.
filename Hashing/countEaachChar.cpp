15655c3773876982c2dc227504f8a363aa74be4d
Mahadev1729 <devcampus17@gmail.com>
2026-01-25 13:07:45 +0530

    Add initial implementation of character counting program and executable

---
+++ Hashing/countEaachChar.cpp
@@ -0,0 +1,20 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve(string s){
+    unordered_map<char,int>mpp;
+    for(auto ch:s){
+        mpp[ch]++;
+    }
+
+    for(auto it:mpp){
+        cout<<it.first<<":"<<it.second<<endl;
+    }
+}
+
+int main(){
+    string s="Mahadev";
+    solve(s);
+    return 0;
+}
