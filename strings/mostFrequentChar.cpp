9939616f159ca1372b9850662fcd6db360185822
Mahadev1729 <devcampus17@gmail.com>
2026-02-11 15:40:00 +0530

    jdfljkkdjkjd

---
+++ strings/mostFrequentChar.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+char solve(string &s){
+    vector<int>v(26,0);
+    char ans;
+    for(int i=0;i<s.size();i++){
+        int a=s[i]-97;
+        v[a]++;
+    }
+    int m=-1;
+
+    for(int i=0;i<v.size();i++){
+       if(m<v[i]){
+        m=v[i];
+        ans=(char)(i+97);
+       }
+    }
+    return ans;
+
+}
+
+int main(){
+   string s="mahadeve";
+   char c=solve(s);
+   cout<<c;
+   return 0;
+}
