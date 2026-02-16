56d4ffa306f9b656705a4941ec85e5d10dd8e96b
Mahadev1729 <devcampus17@gmail.com>
2026-02-16 14:32:23 +0530

    Add decimal_to_binary and longest_common_prefix implementations

---
+++ recursion/decimal_to_binary.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+void decimal_to_binary(int n, string &s){
+    if(n==0){
+        return ;
+    }
+    decimal_to_binary(n/2,s);
+    s+=to_string(n%2);
+}
+
+int main(){
+   int n=57;
+   string res;
+   decimal_to_binary(n,res);
+   cout<<res;
+   return 0;
+}
---
+++ strings/longest_common_prefix.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+string solve(vector<string>s){
+    sort(s.begin(),s.end());
+    string first=s.front();
+    string last=s.back();
+    string res;
+    for(int i=0;i<min(first.length(),last.length());i++){
+        if(first[i]==last[i]){
+            res+=first[i];
+        }else{
+            break;
+        }
+    }
+    return res;
+}
+
+int main(){
+    vector<string>s={"flood","flower","flow"};
+    string ans=solve(s);
+    cout<<ans;
+    return 0;
+}
