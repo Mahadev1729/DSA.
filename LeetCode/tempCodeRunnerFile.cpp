6b338171242aa93466bc6d669b488001c76cb3af
Mahadev1729 <devcampus17@gmail.com>
2026-02-22 01:39:31 +0530

    Add water_bottle.cpp and strinCmp.cpp implementations

--- LeetCode/tempCodeRunnerFile.cpp
+++
@@ -1,1 +0,0 @@
- // if (i > index && arr[i] == arr[i - 1]) continue;
---
+++ LeetCode/water_bottle.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int solution(int numbottles,int numexchange){
+    int consumed=0;
+    while(numbottles>=numexchange){
+        consumed+=numexchange;
+        numbottles-=numexchange;
+        numbottles+=1;
+    }
+    return consumed;
+}
+
+int main(){
+    int res=solution(10,3);
+    cout<<res;
+    return 0;
+}
