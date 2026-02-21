30f6e952935ca92dbafe2d87e2a9cea50b79654f
Mahadev1729 <devcampus17@gmail.com>
2026-02-22 01:53:04 +0530

    Refactor code structure for improved readability and maintainability

--- strings/strinCmp.cpp
+++ strings/strinCmp.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+// "1133332233556";
+
+string solve(string &s){
+    int count=1;
+    string res="";
+    for(int i=1;i<s.length();i++){
+        if(s[i]==s[i-1]){
+            count++;
+        }
+        else{
+           res+=to_string(count)+s[i-1];
+           count=1;
+        }
+    }
+    res+=to_string(count)+s[s.length()-1];
+    return res;
+}
+
 int main(){
-    
+    string s="113332233556";
+    cout<<solve(s);
+    return 0;
+
 }
