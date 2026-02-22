3747c8fa645b7da5c3db132298c0edcaa563f36b
Mahadev1729 <devcampus17@gmail.com>
2026-02-23 00:21:47 +0530

    Add TRWBrute.cpp and TRWOptimal.cpp implementations for water trapping problem

---
+++ array/TRWBrute.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int solve_water(vector<int>&nums){
+    int n=nums.size();
+    int totalWater=0;
+    for(int i=0;i<n;i++){
+        int leftMax=0;
+        int rightMax=0;
+        for(int j=0;j<=i;j++){
+            leftMax=max(leftMax,nums[j]);
+        }
+        for(int j=i;j<n;j++){
+            rightMax=max(rightMax,nums[j]);
+        }
+        int water=min(leftMax,rightMax)-nums[i];
+        if(water>0) totalWater+=water;
+    }
+    return totalWater;
+}
+
+int main(){
+    vector<int>arr={4,2,0,3,2,5};
+    cout<<solve_water(arr);
+    return 0;
+}
