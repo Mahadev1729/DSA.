c6ddbfb0a90876a495baf224f46a7fd52164cc42
Mahadev1729 <devcampus17@gmail.com>
2026-02-23 15:37:27 +0530

    Add CMWBruteforce and TRWOptimal implementations for water trapping problem

---
+++ array/CMWBruteforce.cpp
@@ -0,0 +1,24 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int solve(vector<int>&heights){
+    int n=heights.size();
+    int maxWater=0;
+    for(int i=0;i,n;i++){
+        for(int j=i+1;j<n;i++){
+            int width=j-i;
+            int height=min(heights[i],heights[j]);
+            int area=width*height;
+            maxWater=max(area,maxWater);
+        }
+    }
+    return maxWater;
+}
+
+int main(){
+    vector<int>nums={1,8,6,25,4,8,3,7};
+    int res=solve(nums);
+    cout<<res;
+    return 0;
+}
--- array/TRWOptimal.cpp
+++ array/TRWOptimal.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+int solve(vector<int>&nums){
+    int totalWater=0;
+    int n=nums.size();
+    int l=0;
+    int r=n-1;
+    int lmax=0;
+    int rmax=0;
+    while(l<r){
+        lmax=max(lmax,nums[l]);
+        rmax=max(rmax,nums[r]);
+        if(lmax<rmax){
+            totalWater+=(lmax-nums[l]);
+            l++;
+        }else{
+            totalWater+=(rmax-nums[r]);
+            r--;
+        }
+    }
+    return totalWater;
+}
+
 int main(){
-    
+     vector<int>nums={23,13,45,67,89,90};
+     int res=solve(nums);
+     cout<<res;
+     return 0;
 }
