3f54a80b6c6791862015f5df89086eb68b82ec68
Mahadev1729 <devcampus17@gmail.com>
2026-02-23 16:14:33 +0530

    Add CMWOptimal implementation for water trapping problem

---
+++ array/CMWOptimal.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int>&heights){
+    int n=heights.size();
+    int lp=0;
+    int rp=n-1;
+    int maxWater=0;
+    while(lp<rp){
+        int w=rp-lp;
+        int ht=min(heights[lp],heights[rp]);
+        int area=w*ht;
+        maxWater=max(area,maxWater);
+        heights[lp]<heights[rp]?lp++:rp--;
+    }
+    return maxWater;
+}
+
+int main(){
+    vector<int>heights={1,8,6,25,4,8,3,7};
+    int res=solution(heights);
+    cout<<res;
+    return 0; 
+}
