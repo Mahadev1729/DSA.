de19657e9a626c3236a14a59013b4fbc47eff323
Mahadev1729 <devcampus17@gmail.com>
2026-02-24 13:47:23 +0530

    Refactor whitespace for improved code readability in valid_Parenthisis.cpp

--- STACKSS/valid_Parenthisis.cpp
+++ STACKSS/valid_Parenthisis.cpp
@@ -5,13 +5,11 @@ bool is_Valid_Parenthisis(string s) {
     stack<char> st;
     for (int i = 0; i < s.size(); i++) {
         char ch = s[i];
-
-        if (ch == '(' || ch == '{' || ch == '[') {
+         if (ch == '(' || ch == '{' || ch == '[') {
             st.push(ch);
         } else {
             if (!st.empty()) {
                 char top = st.top();
-
                 if ((ch == ')' && top == '(') ||
                     (ch == '}' && top == '{') ||
                     (ch == ']' && top == '[')) {
