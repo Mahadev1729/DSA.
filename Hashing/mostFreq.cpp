91be6f67534fbb57310d5668727a0d5f8e7fba3f
Mahadev1729 <devcampus17@gmail.com>
2026-03-27 13:30:55 +0530

    some dsa questions

---
+++ Hashing/mostFreq.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int solve(vector<int>&nums){
+    unordered_map<int,int>freq;
+    int maxFreq=INT_MIN;
+    int result;
+    for(int it:nums){
+        freq[it]++;
+        if(freq[it]>maxFreq){
+            maxFreq=nums[it];
+            result=it;
+        }
+    }
+    return result;
+}
+
+int main(){
+   vector<int>nums={1,2,1,1,1,3};
+   int res;
+   res=solve(nums);
+   cout<<res;
+   return 0;
+}
---
+++ Maths/car_intersect.cpp
@@ -0,0 +1,26 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int car_intersect(vector<vector<int>>&nums){
+    unordered_set<int>st;
+    for(auto x:nums){
+        int start=x[0];
+        int end=x[1];
+        for(int i=start;i<=end;i++){
+            st.insert(i);
+        }
+    }
+    return st.size();
+}
+
+int main(){
+    vector<vector<int>>v={
+        {2,3},
+        {1,7},
+        {4,9}
+    };
+    int res=car_intersect(v);
+    cout<<res;
+    return 0;
+}
