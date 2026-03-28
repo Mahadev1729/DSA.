f0f9057b192f10a18d5600b16139bdd7b779587a
Mahadev1729 <devcampus17@gmail.com>
2026-03-29 00:29:08 +0530

    Add implementations for target sum and maximum product algorithms

---
+++ Dynamic Programming/targetSum.cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int solveTargetSum(vector<int>nums,int target){
+    int n=nums.size();
+    unordered_map<int,int>dp;
+    dp.insert({0,1});
+    for(int num:nums){
+        unordered_map<int,int>nextDp;
+        for(auto &it:dp){
+            int sum=it.first;
+            int count=it.second;
+            nextDp[sum+num]+=count;
+            nextDp[sum-num]+=count;
+        }
+        dp=nextDp;
+    }
+    return dp[target];
+}
+
+int main(){ 
+     vector<int>nums={1,1,1,1,1};
+     int res=solveTargetSum(nums,3);
+     cout<<res;
+     return 0;
+}
---
+++ array/maximumPdt.cpp
@@ -0,0 +1,18 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int maxiMumPdt(vector<int>&nums){
+    int maxPdt=INT_MIN;
+    int currentPdt=1;
+    for(int i=0;i<n;i++){
+        currentPdt=currentPdt*nums[i];
+        if(currentPdt<0){
+            currentPdt=1;
+        }
+    }
+}
+
+int main(){
+
+}
