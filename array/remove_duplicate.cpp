b0cbb7f718265b504684be499345da72d6873584
Mahadev1729 <devcampus17@gmail.com>
2026-04-27 23:56:43 +0530

    some

---
+++ array/remove_duplicate.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int remove_duplicate(vector<int>&nums){
+    int n=nums.size();
+    for(int i=1;i<n;i++){
+        if(nums[i]==nums[i-1]){
+            return nums[i];
+            break;
+        }
+    }
+    return -1;
+}
+
+int main(){
+   vector<int>arr={1,2,2,3,4};
+   int ans=remove_duplicate(arr);
+   cout<<ans;
+   return 0;
+}
