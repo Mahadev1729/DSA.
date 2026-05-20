4be29f09735d50d44edbb0e03ed39ec14b619bb8
Mahadev1729 <devcampus17@gmail.com>
2026-05-20 22:05:50 +0530

    Implement countKthroot function and update launch configuration for array directory

--- array/countKthroot.cpp
+++ array/countKthroot.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 
 int solve_root_K(int l,int r,int k){
-    
-}
+    int count=0;
+    int start=1;
+    for(int i=l;i<=r;i++){
+        if(pow(start,k)==i){
+            count++;
+            start++;
+        }
+        
+    }
+    return count;
+}    
+
 
 int main(){
-
+  int res=solve_root_K(1,9,3);
+  cout<<res;
+  return 0;
 }
