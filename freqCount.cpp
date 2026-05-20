9094be44253eddbd0f1da6f22ebadd484d2eff22
Mahadev1729 <devcampus17@gmail.com>
2026-05-20 23:30:56 +0530

    Add frequency count program to calculate and display element frequencies

---
+++ freqCount.cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    vector<int>arr={1,3,2,4,1};
+    int n=arr.size();
+    vector<int>freq(n);
+    for(int i=0;i<n;i++){
+       freq[arr[i]]++;
+    }
+
+    for(int it:freq){
+        cout<<it<<"\n";
+    }
+    return 0;
+}
