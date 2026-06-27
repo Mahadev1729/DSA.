1033dcebb30acf35422dba38c7ef59830759c3d2
Mahadev1729 <devcampus17@gmail.com>
2026-06-27 19:33:22 +0530

    daimond

---
+++ RotationArr/daimond.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // Upper half
+    for (int i = 1; i <= n; i++) {
+
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++)
+            cout << " ";
+
+        // Print stars
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = n - 1; i >= 1; i--) {
+
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++)
+            cout << " ";
+
+        // Print stars
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+
+        cout << endl;
+    }
+
+    return 0;
+}
