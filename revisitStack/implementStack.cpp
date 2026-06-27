3f30dc1b918b2d516997035470ea6bc12dfdf2ef
Mahadev1729 <devcampus17@gmail.com>
2026-06-27 12:39:08 +0530

    some code added

--- revisitStack/implementStack.cpp
+++ revisitStack/implementStack.cpp
@@ -1,37 +1,50 @@
-﻿#include<bits/stdc++.h>
-
+﻿#include <bits/stdc++.h>
 using namespace std;
-int N=100;
-int stack[N];
-int top=-1;
 
+const int N = 100;
+int arr[N];
+int top = -1;
 
-void push(int val){
-    if(top>N-1){
-        cout<<"stack overflow";
+void push(int val) {
+    if (top >= N - 1) {
+        cout << "Stack Overflow\n";
+        return;
     }
-    top++;
-    stack[top]=val;
+
+    arr[++top] = val;
 }
 
-void pop(){
-    if(top==-1){
-        cout<<"Stack underflow";
+void pop() {
+    if (top == -1) {
+        cout << "Stack Underflow\n";
+        return;
     }
-    int data=stack[top];
+
     top--;
 }
-void display(){
-    for(int i=top;i>=0;i--){
-        cout<<stack[i]<<" ";
+
+void display() {
+    if (top == -1) {
+        cout << "Stack is Empty\n";
+        return;
     }
+
+    for (int i = top; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
-int main(){
+int main() {
     push(34);
     push(67);
     push(90);
-    display();
+
+    display();   // 90 67 34
+
+    pop();
+
+    display();   // 67 34
 
     return 0;
 }
---
+++ strings/countArrWithCondition.cpp
@@ -0,0 +1,14 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int min_arr(vector<int>&arr){
+    int min_ele=INT_MAX;
+    for(auto it:arr){
+        
+    }
+}
+
+int main(){
+    
+}
