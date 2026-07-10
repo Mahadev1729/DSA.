25217feb921eb7ec9a10755dfd282576c594ad0f
Mahadev1729 <devcampus17@gmail.com>
2026-07-10 18:42:11 +0530

    add about template in c++

---
+++ Templates/generic.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+
+void display(T value){
+    cout<<"Generic:"<<value<<endl;
+}
+
+template<>
+void display<char>(char value){
+    cout<<"Character:"<<value<<endl;
+}
+int main(){
+   display(10);
+   display('A');
+   return 0;
+}
