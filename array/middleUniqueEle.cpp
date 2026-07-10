8d9d1bfd10d7de6d18c6f854ab1eea5977ce33ec
Mahadev1729 <devcampus17@gmail.com>
2026-07-11 01:43:45 +0530

    code about unique middle element

---
+++ array/middleUniqueEle.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+bool uniqueMiddleEle(vector<int>&nums){
+    map<int,int>mpp;
+    int n=nums.size();
+    int mid=n/2;
+    for(int it:nums){
+      mpp[it]++;
+    }
+    return mpp[nums[mid]]==1;
+}
+int main(){
+   vector<int>arr={1,2,3};
+   bool res=uniqueMiddleEle(arr);
+   cout<<res;
+   return 0;
+}
